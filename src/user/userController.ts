01b7cf08f9c6fbc9765dfca45f94f6f78397de77
viraljain7 <veerljain1234@gmail.com>
2024-06-03 17:36:39 +0530

    Added Password Hashing using bcrypt

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
+import bcrypt from "bcrypt";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -22,6 +23,8 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
 
     return next(error);
   }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
   // process & logic
   // response
   res.json({
